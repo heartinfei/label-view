23c1566fd68d5e480d710158f9565a80348ab4bf
heartinfei@gmail.com <551357>
2015-09-06 14:20:38 +0800

    as

---
+++ testlibrary/src/main/java/com/wq/testlibrary/Test.java
@@ -0,0 +1,10 @@
+package com.wq.testlibrary;
+
+/**
+ * Created by heartinfei on 2015/9/6.
+ */
+public class Test {
+    public static final String say(String name) {
+        return "Hello:" + name;
+    }
+}
